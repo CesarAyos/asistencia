dde4139eee6cd8728bbd6b7b002c42212e8b2474
unknown <ayos007@gmail.com>
2023-08-16 16:31:13 -0400

    asistencia

---
+++ codigo.js
@@ -0,0 +1,34 @@
+let cantidad = prompt("¿cuantos alumnos son?");
+let alumnosTotales = [];
+
+for (i = 0; i < cantidad; i++) {
+    alumnosTotales[i] = [prompt("Nombre del alumno " + (i+1)),0];
+}
+
+const tomarsAsistencia = (nombre,p)=>{
+    let presencia = prompt(nombre);
+    if (presencia == "p" || presencia == "P") {
+        alumnosTotales[p][1]++;
+    }
+}
+
+
+for (i = 0; i < 30; i++) {
+    for (alumno in alumnosTotales) {
+        tomarsAsistencia(alumnosTotales[alumno][0],alumno);
+    }       
+}
+
+
+for (alumno in alumnosTotales) {
+    let resultado = `${alumnosTotales[alumno][0]}:<br>
+    ________presentes: ${alumnosTotales[alumno][1]} <br>
+    ________ausentes: ${30 - parseInt(alumnosTotales[alumno][1])}
+    `;
+    if (30 - alumnosTotales[alumno][1] > 18) {
+        resultado+= "<b> REPROBADO POR INASISTENCIA </b> <br><br>";
+    } else {
+        resultado+= "<br><br>"
+    }
+    document.write(resultado)
+}
